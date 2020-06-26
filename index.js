2fcc174f3ef2e9e8c780cf2c992130b401ea4f2f
devdplx <devdplx@gmail.com>
2020-06-25 21:19:05 -0400

    Formatting for npm

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const { Client, Command } = require('./misako');
-const { betaToken } = require('./config');
-const misako = new Client({
-    owners: ['153277301793095682'],
-    commandPath: `${__dirname}/commands`
-});
-
-misako.on('ready', e => {
-    console.log('connected');
-});
-
-misako.on('message', msg => {
-    misako.handleMessage(msg);
-}); 
-
-misako.login(betaToken);
+module.exports = {
+    Client: require('./client'),
+    Command: require('./commands/base'),
+    Type: require('./types/base')
+};
--- misako/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    Client: require('./client'),
-    Command: require('./commands/base'),
-    Type: require('./types/base')
-};
